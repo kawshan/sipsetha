dac90f94d843f035965e1643f301b6db2de03fe9
kawshan <kawshan.virantha9999@gmail.com>
2024-12-06 23:14:23 +0530

    initial commit AKA version 0

---
+++ src/main/java/lk/sipsetha/SipsethaApplication.java
@@ -0,0 +1,22 @@
+package lk.sipsetha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipsethaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipsethaApplication.class, args);
+
+		System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+		System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+		System.out.println("⭐⭐⭐⭐⭐    application started  ⭐⭐⭐⭐⭐⭐⭐");
+		System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+		System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐");
+
+
+
+	}
+
+}
---
+++ src/test/java/lk/sipsetha/SipsethaApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sipsetha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipsethaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

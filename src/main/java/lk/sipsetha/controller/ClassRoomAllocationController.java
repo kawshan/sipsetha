f37df2adfc3c6a39ca0994c6aa9b202b23464445
kawshan <kawshan.virantha9999@gmail.com>
2025-01-07 10:27:30 +0530

    version 20

--- src/main/java/lk/sipsetha/controller/ClassRoomAllocationController.java
+++ src/main/java/lk/sipsetha/controller/ClassRoomAllocationController.java
@@ -1,13 +1,13 @@
 package lk.sipsetha.controller;
 
+import lk.sipsetha.dao.AllocationStatusDao;
 import lk.sipsetha.dao.ClassRoomAllocationDao;
+import lk.sipsetha.entity.AllocationStatus;
 import lk.sipsetha.entity.ClassroomAllocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,6 +18,9 @@ public class ClassRoomAllocationController {
 
     @Autowired
     private ClassRoomAllocationDao dao;
+
+    @Autowired
+    private AllocationStatusDao allocationStatusDao;
 
     @GetMapping(value = "/findall")
     public List<ClassroomAllocation> getAllClassRoomAllocation(){
@@ -33,5 +36,52 @@ public class ClassRoomAllocationController {
         classroomAllocationView.addObject("title","classroom allocation ui");
         return classroomAllocationView;
     }
+
+    @PostMapping
+    public String saveClassRoomAllocation(@RequestBody ClassroomAllocation classroomAllocation){
+        //authentication and authorization
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        //existing and duplicate
+        //operator
+        try {
+            dao.save(classroomAllocation);
+            return "ok";
+        }catch (Exception e){
+            return "class room allocation save is not complete "+e.getMessage();
+        }
+
+    }
+
+    @PutMapping
+    public String updateClassRoomAllocation(@RequestBody ClassroomAllocation classroomAllocation){
+        //authentication and authorization
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        //existing and duplicate
+        //operator
+        try {
+            dao.save(classroomAllocation);
+            return "ok";
+        }catch (Exception e){
+            return "update class room allocation is not complete"+e.getMessage();
+        }
+
+    }
+
+
+    @DeleteMapping
+    public String deleteClassroomAllocation(@RequestBody ClassroomAllocation classroomAllocation){
+        //authentication and authorization
+        //existing
+        //operator
+        try {
+            AllocationStatus deleteAllocationStatus =allocationStatusDao.getReferenceById(2);
+            classroomAllocation.setAllocationstatus_id(deleteAllocationStatus);
+            dao.save(classroomAllocation);
+            return "ok";
+        }catch (Exception e){
+            return "delete class room allocation is not complete "+e.getMessage();
+        }
+    }
+
 
 }
--- src/main/resources/static/controllerjs/classroomallocation.js
+++ src/main/resources/static/controllerjs/classroomallocation.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',()=>{
     //call refresh class room allocation
-    refreshClassRoomAllocation()
+    refreshClassRoomAllocationForm()
 
     //call refresh class room allocation table
     refreshClassRoomAllocationTable();
@@ -17,7 +17,8 @@ const refreshClassRoomAllocationTable = ()=>{
         {dataType:'function',propertyName:getAllocationType},
         {dataType:'function',propertyName:getWeekday},
         {dataType:'function',propertyName:getClassHall},
-        {dataType:'function',propertyName:getClassOffering}
+        {dataType:'function',propertyName:getClassOffering},
+        {dataType:'function',propertyName:getAllocationStatus}
     ];
 
 
@@ -26,7 +27,7 @@ const refreshClassRoomAllocationTable = ()=>{
 
 
 //define refresh class room allocation form
-const refreshClassRoomAllocation = ()=>{
+const refreshClassRoomAllocationForm = ()=>{
     classRoomAllocation = new Object();
     classroomAllocationForm.reset()
 
@@ -76,3 +77,203 @@ return ob.classhall_id.name;
 const getClassOffering = (ob)=>{
 return ob.classoffering_id.classname;
 }
+
+//define function for get allocation status
+const getAllocationStatus = (ob)=>{
+    return ob.allocationstatus_id.name;
+}
+
+
+//define function for class room refill
+const classRoomAllocationRefill = (ob,rowOb)=>{
+    $('#modalClassRoomAllocationAdd').modal('show');
+    classRoomAllocation=JSON.parse(JSON.stringify(ob))
+    oldClassRoomAllocation = JSON.parse(JSON.stringify(ob));
+
+    selectStartTime.value=classRoomAllocation.starttime
+    selectEndTime.value=classRoomAllocation.endtime
+
+
+    fillDataIntoSelect(selectAllocationStatus,'select allocation status',allocationStatues,'name',classRoomAllocation.allocationstatus_id.name);
+
+    fillDataIntoSelect(selectAllocationType,"select allocation type",allocationTypes,'name',classRoomAllocation.allocationtype_id.name);
+
+    fillDataIntoSelect(selectWeekDay,"select week day",weekdays,'name',classRoomAllocation.weekday_id.name);
+
+    fillDataIntoSelect(selectClassHall,'select class hall',classHalls,'name',classRoomAllocation.classhall_id.name);
+
+    fillDataIntoSelect(selectClassOffering,'select class offering',classOfferings,'classname',classRoomAllocation.classoffering_id.clssname);
+
+
+
+    if (classRoomAllocation.note!=null){
+        textNote.value=classRoomAllocation.note;
+    }else {
+        textNote.value="";
+    }
+
+
+
+}
+
+
+
+//define function for check errors
+const checkErrors =()=>{
+    let errors="";
+
+    if (classRoomAllocation.starttime == null){
+        errors=errors+"select start time cannot be empty \n"
+    }
+    if (classRoomAllocation.endtime == null){
+        errors=errors+"end time cannot be empty \n";
+    }
+    if (classRoomAllocation.allocationstatus_id == null){
+        errors=errors+"allocation status cannot be empty \n";
+    }
+    if (classRoomAllocation.allocationtype_id == null){
+        errors=errors+"allocation type cannot be empty \n"
+    }
+    if (classRoomAllocation.weekday_id == null){
+        errors=errors+"weekday cannot be empty \n";
+    }
+    if (classRoomAllocation.classhall_id == null){
+        errors=errors+"class hall cannot be empty \n";
+    }
+    if (classRoomAllocation.classoffering_id == null){
+        errors=errors+"class offering cannot be empty \n";
+    }
+
+    return errors;
+}
+
+//define class room allocation submit button
+const classRoomAllocationSubmit = ()=>{
+    let errors = checkErrors();
+    if (errors == ""){
+        const userConfirm = confirm("are you sure to add following class room allocation \n"
+        +'\n start time is '+classRoomAllocation.starttime
+        +'\n end time is '+classRoomAllocation.endtime
+        +'\n allocation status is '+classRoomAllocation.allocationstatus_id.name
+        +'\n allocation type is '+classRoomAllocation.allocationtype_id.name
+        +'\n weekday is '+classRoomAllocation.weekday_id.name
+        +'\n class hall is '+classRoomAllocation.classhall_id.name
+        +'\n class offering is '+classRoomAllocation.classoffering_id.classname
+
+        );
+        if (userConfirm){
+            let postServiceResponse = ajaxPostRequest("/classroomallocation",classRoomAllocation);
+            if (postServiceResponse == "ok"){
+                alert("save success "+postServiceResponse);
+
+                refreshClassRoomAllocationForm();
+                $('#modalClassRoomAllocationAdd').modal('hide');
+                refreshClassRoomAllocationTable();
+            }else {
+                alert("save not successful "+postServiceResponse);
+            }
+        }
+    }
+}
+
+//define function for check updates
+const checkUpdates = ()=>{
+    let updates = "";
+
+    if (classRoomAllocation.starttime != oldClassRoomAllocation.starttime){
+        updates=updates+"start time is changed \n"
+    }
+    if (classRoomAllocation.endtime != oldClassRoomAllocation.endtime){
+        updates=updates+"end time is changed \n";
+    }
+    if (classRoomAllocation.allocationstatus_id.name != oldClassRoomAllocation.allocationstatus_id.name){
+        updates=updates+"allocation status is changed \n";
+    }
+    if (classRoomAllocation.allocationtype_id.name != oldClassRoomAllocation.allocationtype_id.name){
+        updates=updates+"allocation type is changed";
+    }
+    if (classRoomAllocation.weekday_id.name != oldClassRoomAllocation.weekday_id.name){
+        updates=updates+"weekday is changed \n";
+    }
+    if (classRoomAllocation.classhall_id.name != oldClassRoomAllocation.classhall_id.name){
+        updates=updates+"weekday is changed \n";
+    }
+    if (classRoomAllocation.classoffering_id.classname != oldClassRoomAllocation.classoffering_id.classname){
+        updates=updates+"class offering is changed \n";
+    }
+    if (classRoomAllocation.note != oldClassRoomAllocation.note){
+        updates=updates+"note is changed \n";
+    }
+
+    return updates;
+}
+
+//define class room allocation update button function
+const updateClassRoomAllocation = ()=>{
+    let errors = checkErrors();
+    if (errors == ""){
+        let updates = checkUpdates();
+        if (updates==""){
+            alert("nothing to update")
+        }else {
+            let userConfirm =confirm("are you sure to apply following updates \n"+updates);
+            if (userConfirm){
+                let putServiceResponse = ajaxPutRequest("/classroomallocation",classRoomAllocation);
+                if (putServiceResponse=="ok"){
+                    alert("update successful "+putServiceResponse);
+                    refreshClassRoomAllocationForm();
+                    $('#modalClassRoomAllocationAdd').modal('hide');
+                    refreshClassRoomAllocationTable();
+                }else {
+                 alert("update not successful "+putServiceResponse);
+                }
+            }
+        }
+    }
+}
+
+//define function for delete classroom allocation
+const deleteClassRoomAllocation = (ob,rowIndex)=>{
+    console.log("delete"+ob+rowIndex);
+    tableClassRoomAllocation.children[1].children[rowIndex].style.background="pink";
+    setTimeout(function (){
+        const userConfirm = confirm("are you sure to delete following class room allocation \n"
+        +'\n start time is'+ob.starttime
+        +'\n end time is'+ob.endtime
+        +'\n allocation status  is'+ob.allocationstatus_id.name
+        +'\n allocation type  is'+ob.allocationtype_id.name
+        +'\n weekday  is'+ob.weekday_id.name
+        +'\n class hall  is'+ob.classhall_id.name
+        +'\n class offering  is'+ob.classoffering_id.classname
+        )
+        if (userConfirm){
+            let deleteServiceResponse = ajaxDeleteRequest("/classroomallocation",ob);
+            if (deleteServiceResponse=="ok"){
+                alert("delete successful "+deleteServiceResponse);
+                refreshClassRoomAllocationTable();
+            }else {
+                alert("delete not successful "+deleteServiceResponse);
+            }
+        }
+        refreshClassRoomAllocationTable();
+    },500)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

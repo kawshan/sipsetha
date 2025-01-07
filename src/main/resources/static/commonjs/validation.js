3ef47c782cfbf0e14b943f05a316a721851a2aaa
kawshan <kawshan.virantha9999@gmail.com>
2025-01-07 11:49:58 +0530

    version 46

--- src/main/resources/static/commonjs/validation.js
+++ src/main/resources/static/commonjs/validation.js
@@ -101,9 +101,60 @@ const dataListValidator = (fieldId,dataListName,object,property,oldObject,displa
     }
 }
 
+//start validation of two values
+const textValidatorWithTwoValues=(fieldId,pattern,object,property,oldObject)=>{
+    const fieldValue = fieldId.value;
+    const regPattern = new RegExp(pattern);
+
+    if (fieldValue !== ""){
+        if (regPattern.test(fieldValue)){
+            //bind value into object property
+            window[object][property]=fieldValue;
+
+            if (window[oldObject]!=null && window[object][property]!=window[oldObject][property]){
+                // fieldId.style.border="2px solid #e67e22";
+                fieldId.style.backgroundColor="#f1c40f";
+            }else {
+                fieldId.style.border="2px solid green";
+            }
+        }else {
+            fieldId.style.border="2px solid red";
+            window[object][property]=null;  //need to bind null
+        }
+    }else {
+        if (fieldId.required){
+            window[object][property]=null;
+            fieldId.style.border="2px solid red";
+        }else {
+            fieldId.style.border = '2px solid #ced4da';
+        }
+    }
 
 
 
+}
+
+const selectDBFieldValidatorWithTwoValues = (fieldId,pattern,object,property,oldObject)=>{
+    const fieldValue = fieldId.value;
+    if (fieldValue!==""){
+        window[object][property]=JSON.parse(fieldValue);//convert js object   //json parse eken venne string eka java script object ekat convert vena eka
+                                                        //JSON.stringify is a JavaScript method used to convert a JavaScript object or value to a JSON string
+        if (window[oldObject]!=null && window[object][property]['id']!= window[oldObject][property]['id']){
+            // fieldId.style.border="2px solid #e67e22";
+            fieldId.style.backgroundColor="#f1c40f";
+        }else {
+            fieldId.style.border="2px solid green";
+        }
+    }else {
+        window[object][property]=null;
+        if (fieldId.required){
+            fieldId.style.border="2px solid red";
+        }else {
+            fieldId.style.border="2px solid #ced4da";
+        }
+    }
+
+}
 
 
 
